3c0726a6f86a49c3550b4e70edc66290729f797b
naelsondouglas <naelson17@gmail.com>
2022-06-30 10:22:57 -0300

    started using NodeTransformer

--- src/engine.py
+++ src/engine.py
@@ -1,23 +1,19 @@
 import ast
 from lint_warnings import cui
 
-no = None
-
-def test_srs(src:str):
-    return ast.dump(ast.parse(src, mode='eval'), indent=4)
-
+class Visitor(ast.NodeTransformer):
+    def visit_BoolOp(self, node:ast.AST):
+        fixed = node
+        if cui.is_comparing_boolop(node):
+            fixed = cui.fix(node)
+        self.generic_visit(node)
+        return fixed
 
 if __name__ == '__main__':
     with open('samples/cui.py') as f:
         text = f.read()
 
-    for node in ast.walk(ast.parse(text)):
-        if type(node) == ast.BoolOp:
-            if len(node.values) > 2:
-                no = node
-                d = ast.dump(no, indent=4)
-                print(d)
-                print(10*'-')
-            if cui.is_comparing_boolop(node):
-                fixed = cui.fix(node)
-                print('comparou')
+    tree = ast.parse(text)
+    result = Visitor().visit(tree)
+    result = ast.unparse(result)
+    print(result)
