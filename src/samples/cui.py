a618e8b518dabb9651df02918badd132037e82d1
naelsondouglas <naelson17@gmail.com>
2022-06-30 03:31:33 -0300

    initial changes added

--- src/samples/cui.py
+++ src/samples/cui.py
@@ -1,4 +1,4 @@
-if value == 1 or value == '1' or value == 'um' or value == 'one' or value == '1.0' or value == '1.00' or value == '1.000' or foo == 10 or foo == 10.1:
+if value == 1 or value == '1' or value == 'um' or value == 'one' or value == '1.0' or value == '1.00' or value == '1.000' or foo == 10 or foo == 10.1 or is_true():
     print('Ok')
 else:
     print('Not ok')
